f0d8af3292a4a4ed766296ab5c602e0e4e7bf3b9
Islam Rostom <islam.rostom93@gmail.com>
2021-01-11 15:55:30 +0200

    Update xpay.spec.ts

--- test/xpay.spec.ts
+++ test/xpay.spec.ts
@@ -1,13 +1,12 @@
 import { AxiosWrapper } from "../src/axiosWrapper";
 import { PaymentMethod, ServerSetting } from "../src/interfaces/payments";
 import { Xpay } from "../src/xpay";
-
 jest.mock("../src/AxiosWrapper");
-const mockedAxios = AxiosWrapper as jest.Mocked<typeof AxiosWrapper>;
+const mockedAxiosWrapper = AxiosWrapper as jest.Mocked<typeof AxiosWrapper>;
 
 describe("preparePayment method execute successfuly", () => {
   beforeAll(() => {
-    mockedAxios.prepareAmount.mockResolvedValue({
+    mockedAxiosWrapper.prepareAmount.mockResolvedValue({
       total_amount: 50,
       total_amount_currency: "EGP",
       KIOSK: { total_amount: 50, total_amount_currency: "EGP" },
@@ -60,13 +59,13 @@ describe("preparePayment method execute successfuly", () => {
 
 describe("makePayment method execute successfuly", () => {
   beforeAll(() => {
-    mockedAxios.prepareAmount.mockResolvedValue({
+    mockedAxiosWrapper.prepareAmount.mockResolvedValue({
       total_amount: 50,
       total_amount_currency: "EGP",
       KIOSK: { total_amount: 50, total_amount_currency: "EGP" },
     });
 
-    mockedAxios.pay.mockResolvedValue({
+    mockedAxiosWrapper.pay.mockResolvedValue({
       iframe_url: null,
       transaction_id: 4647,
       transaction_status: "PENDING",
@@ -128,7 +127,7 @@ describe("makePayment method execute successfuly", () => {
         ]
       )
       .then((data) => {
-        expect(mockedAxios.pay).toHaveBeenCalledWith(
+        expect(mockedAxiosWrapper.pay).toHaveBeenCalledWith(
           {
             community_id: payment.communityId,
             variable_amount_id: payment.apiPaymentId,
@@ -167,7 +166,7 @@ describe("makePayment method execute successfuly", () => {
         phone_number: "+201111111111",
       })
       .then((data) => {
-        expect(mockedAxios.pay).toHaveBeenCalledWith(
+        expect(mockedAxiosWrapper.pay).toHaveBeenCalledWith(
           {
             community_id: payment.communityId,
             variable_amount_id: payment.apiPaymentId,
@@ -210,7 +209,7 @@ describe("makePayment method execute successfuly", () => {
 
 describe("getTransaction method execute successfuly", () => {
   beforeAll(() => {
-    mockedAxios.getTransactionInfo.mockResolvedValue({
+    mockedAxiosWrapper.getTransactionInfo.mockResolvedValue({
       created: "2020-12-02T17:05:43.020966+02:00",
       id: 4442,
       uuid: "c3b2c863-55e2-4411-aea8-de0bc01a6080",
@@ -260,7 +259,7 @@ describe("getTransaction method execute successfuly", () => {
 
 describe("getTransaction method execute successfuly", () => {
   beforeAll(() => {
-    mockedAxios.getTransactionInfo.mockResolvedValue({
+    mockedAxiosWrapper.getTransactionInfo.mockResolvedValue({
       created: "2020-12-02T17:05:43.020966+02:00",
       id: 4442,
       uuid: "c3b2c863-55e2-4411-aea8-de0bc01a6080",
@@ -308,13 +307,13 @@ describe("getTransaction method execute successfuly", () => {
   });
 });
 
-describe.only("makePayment method throws", () => {
+describe("makePayment method throws", () => {
   beforeAll(() => {
-    mockedAxios.prepareAmount.mockResolvedValue({
+    mockedAxiosWrapper.prepareAmount.mockResolvedValue({
       KIOSK: { total_amount: 50, total_amount_currency: "EGP" },
     });
 
-    mockedAxios.pay.mockResolvedValue({
+    mockedAxiosWrapper.pay.mockResolvedValue({
       iframe_url: null,
       transaction_id: 4647,
       transaction_status: "PENDING",
@@ -395,5 +394,62 @@ describe.only("makePayment method throws", () => {
     ).toThrow(
       'payment method selected: "card" is not available in your community active payment methods: [kiosk]'
     );
+  });
+});
+
+describe("Xpay class rethrow axioWrapper errors", () => {
+  beforeAll(() => {
+    mockedAxiosWrapper.prepareAmount.mockRejectedValue(
+      JSON.stringify({
+        community_id: ["Not found"],
+      })
+    );
+
+    mockedAxiosWrapper.pay.mockRejectedValue(
+      "server responsed with the following code: 401 and the following message: Unauthorized"
+    );
+  });
+
+  test("community id is not found", () => {
+    let payment = new Xpay(
+      "Cce74Y3B.J0P4tItq7hGu2ddhCB0WF5ND1eTubkpT",
+      "m2J7eBK",
+      60
+    );
+
+    return payment.preparePayment(50).catch((e) => {
+      expect(e).toStrictEqual(
+        JSON.stringify({
+          community_id: ["Not found"],
+        })
+      );
+    });
+  });
+
+  test("unauthorized", async () => {
+    mockedAxiosWrapper.prepareAmount.mockResolvedValue({
+      total_amount: 50,
+      total_amount_currency: "EGP",
+      KIOSK: { total_amount: 50, total_amount_currency: "EGP" },
+    });
+
+    let payment = new Xpay(
+      "Cce74Y3B.J0P4tItq7hGu2ddhCB0WF5ND1eTubk",
+      "m2J7eBK",
+      60
+    );
+    await payment.preparePayment(50);
+
+    return payment
+      .makePayment(PaymentMethod.KIOSK, {
+        name: "John Doe",
+        email: "support@xpay.app",
+        phone_number: "+201111111111",
+      })
+      .catch((e) => {
+        expect(e).toStrictEqual(
+          "server responsed with the following code: 401 and the following message: Unauthorized"
+        );
+      });
   });
 });
